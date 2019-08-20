dc89c3ebc24e784289ad2f39ccdd775376803330
Paul Canham <paul@canham.io>
2019-08-20 09:34:46 +0100

    adding in KMS aliases
    Adding in aliases for making the keys flag up that they are for the EKK stack

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -1,18 +1,32 @@
 resource "aws_kms_key" "s3_logging_kms_key" {
-  count = var.s3_kms_key_arn == "" ? 1 : 0
+  count       = var.s3_kms_key_arn == "" ? 1 : 0
+  description = "EKK - S3 Logging KMS Key"
+}
 
-  description = "S3 Logging KMS Key - Created by Terraform"
+resource "aws_kms_alias" "s3_logging_kms_alias" {
+  count         = var.s3_kms_key_arn == "" ? 1 : 0
+  name          = "alias/ekk/s3"
+  target_key_id = aws_kms_key.s3_logging_kms_key[0].key_id
 }
 
 resource "aws_kms_key" "es_kms_key" {
-  count = var.es_kms_key_id == "" || es_encryption_at_rest == 1  ? 1 : 0
+  count       = var.es_kms_key_id == "" || var.es_encryption_at_rest == 1 ? 1 : 0
+  description = "EKK - ES data KMS Key"
+}
 
-  description = "ES data KMS Key - Created by Terraform"
+resource "aws_kms_alias" "es_kms_alias" {
+  count         = var.es_kms_key_id == "" || var.es_encryption_at_rest == 1 ? 1 : 0
+  name          = "alias/ekk/es"
+  target_key_id = aws_kms_key.es_kms_key[0].key_id
 }
 
 resource "aws_kms_key" "kinesis_stream_kms_key" {
-  count = var.ekk_kinesis_stream_kms_key_id == "" ? 1 : 0
-
-  description = "Kinesis Stream KMS Key - Created by Terraform"
+  count       = var.ekk_kinesis_stream_kms_key_id == "" ? 1 : 0
+  description = "EKK - Kinesis Stream KMS Key"
 }
 
+resource "aws_kms_alias" "kinesis_stream_kms_alias" {
+  count         = var.ekk_kinesis_stream_kms_key_id == "" ? 1 : 0
+  name          = "alias/ekk/kinesis_stream"
+  target_key_id = aws_kms_key.kinesis_stream_kms_key[0].key_id
+}
